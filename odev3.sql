c45f526d8c0698d8c093bc2fbd6545dffd79ac80
brkybzkrt <b9bozkurt959@gmail.com>
2021-10-03 16:13:07 +0300

    Create odev3.sql

---
+++ odev3.sql
@@ -0,0 +1,16 @@
+--1
+--select * from country
+--where country like 'A%a'
+
+
+--2
+--select * from country
+--where country ilike '_____%n'
+
+--3
+--select * from film
+--where title ilike '%t%t%t%t%'
+
+--4
+--select * from film
+--where title like 'C%' and length >90 and rental_rate=2.99
